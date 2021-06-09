170ababd3fb757e73405fa7e240e33ead7652bfe
Nayem <nayemk900@gmail.com>
2021-06-10 02:49:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "tailwindcss/tailwind.css"
+import Header from './Components/Home/Header/Header';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
