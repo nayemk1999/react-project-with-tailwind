dda59eead91b1ce066e3102c7b12d73012169b44
Nayem <nayemk900@gmail.com>
2021-06-10 03:14:48 +0600

    navbar container set

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -19,7 +19,7 @@ export default function NavBar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="2xl:container xl:container mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="2xl:container xl:container  px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
