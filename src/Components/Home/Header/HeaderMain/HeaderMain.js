9409b0cb4147a469266eeebf78aab98157bd6cc8
Nayem <nayemk900@gmail.com>
2021-06-10 23:51:50 +0600

    Header Main section add

--- src/Components/Home/Header/HeaderMain/HeaderMain.js
+++ src/Components/Home/Header/HeaderMain/HeaderMain.js
@@ -3,13 +3,13 @@ import React from 'react';
 const HeaderMain = () => {
     return (
         <div className="2xl:container 2xl:mx-auto xl:mx-auto xl:container px-2 sm:px-6 lg:px-20">
-            <div className="flex items-center justify-center ">
-                <div>
-                    <h1>Now is the time to start selling things online</h1>
-                    <p>Kora is a leading online shop creation platform which offers amazing features for ecommerce</p>
-                    <div>
-                        <button type="button">Sign up</button>
-                        <button type="button">Discover</button>
+            <div className="flex lg:flex-row items-center justify-center mt-3 md:flex-col sm: flex-col ">
+                <div className=" sm: mb-2">
+                    <h1 className="lg:text-4xl pl-11 pt-11 font-bold sm:text-sm">Now is the time <br/> to start selling <br/>things online</h1>
+                    <p className="pl-11 mt-2">Kora is a leading online shop creation platform which offers amazing features for ecommerce</p>
+                    <div className='mt-11 pl-11'>
+                        <button className="p-4 bg-red-500 mr-2 text-2xl rounded-2xl text-white hover:bg-black" type="button">Sign up</button>
+                        <button className="p-4 bg-red-500 mr-2 text-2xl rounded-2xl text-white hover:bg-black" type="button">Discover</button>
                     </div>
                 </div>
                 <div>
